dbdb278cf2ecdf1d6fa406683099258c0b90e335
Dherrom0706 <dherrom0706@g.educaand.es>
2023-02-24 16:34:16 +0100

    arreglado navigation drawer con añadir y borrado, falta añadir el fragment con la busqueda

--- app/src/main/java/www/iesmurgi/u9_proyprofesoressqlite/MainActivity.kt
+++ app/src/main/java/www/iesmurgi/u9_proyprofesoressqlite/MainActivity.kt
@@ -10,9 +10,11 @@ import android.view.View
 import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.navigation.NavigationView
 import www.iesmurgi.u9_proyprofesoressqlite.databinding.ActivityMainBinding
 
@@ -51,19 +53,37 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
         toogle = ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
         drawer.addDrawerListener(toogle)
 
+        toogle.syncState()
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
 
         val navigationView: NavigationView = findViewById(R.id.navigation_view)
+        navigationView.bringToFront() //importante esta linea, sino no hace nada
         navigationView.setNavigationItemSelectedListener(this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
         when(item.itemId){
-            R.id.menu_item1 -> Toast.makeText(this,"Item 1 seleccionado",Toast.LENGTH_SHORT).show()
-            R.id.menu_item2 -> Toast.makeText(this,"Item 2 seleccionado",Toast.LENGTH_SHORT).show()
-            R.id.menu_item3 -> Toast.makeText(this,"Item 3 seleccionado",Toast.LENGTH_SHORT).show()
+            R.id.menu_aniadir -> {
+                startActivity(Intent(this, AddUpdateActivity::class.java))
+            }
+            R.id.menu_buscar -> {
+                Toast.makeText(this,"Item 2 seleccionado",Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_borrar -> {
+                Toast.makeText(this,"ha seleccionado borrar",Toast.LENGTH_SHORT).show()
+                val alertDialog: MaterialAlertDialogBuilder = MaterialAlertDialogBuilder(this)
+                alertDialog.setMessage("¿Esta seguro que desea borrar todos los datos?")
+                alertDialog.setPositiveButton("Si"){ dialog, which ->
+                    for (i in 0 until lista.size){
+                        onItemDelete(i)
+                    }
+                }
+                alertDialog.setNegativeButton("Cancelar",null)
+                alertDialog.show()
+            }
         }
 
         drawer.closeDrawer(GravityCompat.START)
